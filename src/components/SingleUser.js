dcc5e56d73c13c420998edc66f666975dcb90f12
Mariusz Sygnowski <sygnowski.m@gmail.com>
2019-02-02 19:44:51 +0000

    Filter users from the list if they dont have a name - is working but not used filer from graphQL, will find out later how to do it

--- src/components/SingleUser.js
+++ src/components/SingleUser.js
@@ -1,14 +1,19 @@
 import React, { Fragment } from "react";
 
 export default function SingleUser({ user: { name, role, createdAt } }) {
-  return (
-    <div style={{ border: "solid" }}>
-      <h2>{name}</h2>
-      {role.map(item => (
-        <div key={item}>
-          <h2>{item}</h2>
-        </div>
-      ))}
-    </div>
-  );
+  if (name === "") {
+    // console.log("ok");
+    return <Fragment />;
+  } else {
+    return (
+      <div style={{ border: "solid" }}>
+        <h2>{name}</h2>
+        {role.map(item => (
+          <div key={item}>
+            <h2>{item}</h2>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
