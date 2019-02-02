0ddb8a28b128c8415ea2acf9b4bdb48d37667d3d
Mariusz Sygnowski <sygnowski.m@gmail.com>
2019-02-02 13:52:15 +0000

    inital setup in App.js and added concurrently in package.json: npm run dev will run client and server

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import React, { Component } from "react";
 import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
 
 import logo from "./acre-logo.svg";
 import "./App.css";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/"
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <header className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
+      <ApolloProvider client={client}>
+        <div className="app">
+          <header className="app-header">
+            <img src={logo} className="app-logo" alt="logo" />
 
-          <h1>Welcome to acre</h1>
+            <h1>Welcome to acre</h1>
 
-          <h2>Users</h2>
+            <h2>Users</h2>
 
-          <select>
-            <option value="ADMIN">Admin</option>
-            <option value="ADMIN">Broker</option>
-            <option value="ADVISOR">Advisor</option>
-          </select>
+            <select>
+              <option value="ADMIN">Admin</option>
+              <option value="ADMIN">Broker</option>
+              <option value="ADVISOR">Advisor</option>
+            </select>
 
-          <ul>
-            <li>
-              John <strong>Admin</strong>
-            </li>
-            <li>
-              Mary <strong>Admin</strong>
-            </li>
-          </ul>
-        </header>
-      </div>
+            <ul>
+              <li>
+                John <strong>Admin</strong>
+              </li>
+              <li>
+                Mary <strong>Admin</strong>
+              </li>
+            </ul>
+          </header>
+        </div>
+      </ApolloProvider>
     );
   }
 }
