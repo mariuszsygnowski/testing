8bf57c92ba57d3aea0830f62f2db8d53005f749e
Mariusz Sygnowski <sygnowski.m@gmail.com>
2019-02-17 09:57:37 +0000

    a

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: MiniCssExtractPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: "../"
+            }
+          },
+          "css-loader"
+        ]
+      }
+    ]
+  }
+};
